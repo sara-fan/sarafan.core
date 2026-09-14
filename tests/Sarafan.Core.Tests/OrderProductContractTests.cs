961a08366e57bde9efa0600861dbce480a3e64f7
Maxim Samsonov <m.samsonov@computer.org>
2026-09-14 10:45:35 +0300

    fix

--- tests/Sarafan.Core.Tests/OrderProductContractTests.cs
+++ tests/Sarafan.Core.Tests/OrderProductContractTests.cs
@@ -62,7 +62,7 @@ public sealed class OrderProductContractTests
         using (Assert.EnterMultipleScope())
         {
             Assert.That(order.CreatedAt, Is.EqualTo(CreatedAt));
-            Assert.That(order.UpdatedAt, Is.EqualTo(CreatedAt.AddTicks(1)));
+            Assert.That(order.UpdatedAt, Is.EqualTo(CreatedAt.AddTicks(10)));
         }
     }
 
